d7b8a685afd2ca2bd9f713983afe658a05b98830
bostroemc <carl.bostrom@boschrexroth-us.com>
2022-02-14 17:06:52 -0600

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup
 
 setup(
     name='rfs-parts-db',
-    version='1.2.9',
+    version='1.3.0',
     description='Python-based parts database handler for Rexroth ctrlX AUTOMATION RFS platform',
     author='bostroemc',
     install_requires = ['ctrlx-datalayer', 'ctrlx_fbs', 'jsonschema'],    
     packages=['app'],
-    # https://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
-    # package_data={'./': ['sampleSchema.bfbs']},
     scripts=['main.py'],
     license='Copyright (c) 2020-2022 Bosch Rexroth AG, Licensed under MIT License'
 )
